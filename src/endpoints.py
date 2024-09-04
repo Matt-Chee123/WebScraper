64a604cde8b7651435f1d4abbcf219118b20a8ce
Matt-Chee123 <cheematthew10@gmail.com>
2024-09-04 18:35:56 +0100

    now accepts google popups and scrolls to reveal more than 10 jobs

--- src/endpoints.py
+++ src/endpoints.py
@@ -3,45 +3,51 @@ from flask_cors import CORS  # Import the CORS module
 from bs4 import BeautifulSoup
 import requests
 import urllib.parse
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.select import Select
+import random
+
+import time
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/scrape/google', methods=['POST'])
-def scrape():
+def scrapeGoogle():
+
     data = request.json
     query = data.get('description')
     if not query:
         return jsonify({'error': 'Google Query is required'}), 400
 
+    chrome_options = Options()
+    chrome_options.add_argument('--disable-notifications')
+
     # URL encode the query
     encoded_query = urllib.parse.quote_plus(query)
-    google_url = f"https://www.google.com/search?q={encoded_query}&sca_esv=1c4a2bf3bd651b6a&rlz=1C1AVFC_en-GBGB850GB850&sxsrf=ADLYWIKNp2n0T1vGmA-5LPyrD1j1gLdZAQ%3A1725461526607&ei=FnTYZuvYJImRhbIPtJen2AQ&ved=2ahUKEwi24cOjzamIAxXqW0EAHYfJCXwQ3L8LegQIIRAM&uact=5&oq=jobs%20london%20graduate%20software%20developer&gs_lp=Egxnd3Mtd2l6LXNlcnAiJ2pvYnMgbG9uZG9uIGdyYWR1YXRlIHNvZnR3YXJlIGRldmVsb3BlcjIKEAAYsAMY1gQYRzINEAAYsAMY1gQYRxjJAzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIOEAAYgAQYsAMYkgMYigVIlApQAFgAcAN4AZABAJgBAKABAKoBALgBA8gBAPgBApgCA6ACDZgDAIgGAZAGCZIHATOgBwA&sclient=gws-wiz-serp&jbr=sep:0&udm=8"
-
+    s = Service('C:/Program Files/chromedriver-win64/chromedriver.exe')
+    driver = webdriver.Chrome(service=s, options=chrome_options)
+    time.sleep(2)
+    driver.maximize_window()
+    url='https://www.google.com/search?q=jobs%20london%20graduate%20software%20developer&jbr=sep:0&udm=8&ved=2ahUKEwip7Iq026mIAxXFWUEAHTNoAccQ3L8LegQIIRAM'
+    driver.get(url)
+    time.sleep(3)
+    driver.find_element(By.XPATH,"//*[@id='W0wltc']/div").click()
+    time.sleep(3)
+    for i in range(10):
+        driver.execute_script('window.scrollBy(0, 3000)')
+        time.sleep(random.randint(1,5))
     try:
-        # Make the request with a user-agent header
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'
-        }
-        response = requests.get(google_url, headers=headers)
-        response.raise_for_status()
-
-        # Parse the response content
-        soup = BeautifulSoup(response.text, 'html.parser')
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
         specific_div = soup.find('div', class_='ZNyqGc')
+        jobs=[]
 
-        # Extract headings (h1, h2, h3)
-        if not specific_div:
-            return jsonify({'error': 'Specific div not found'}), 404
-
-        # Extract all hrefs within <a> tags inside the specific <div>
-        jobs = []
         for a_tag in specific_div.find_all('a', href=True):
             jobs.append(a_tag['href'])
-        return jsonify({'jobs': jobs})
 
+        return jsonify({'jobs': jobs})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-
-if __name__ == '__main__':
-    app.run(debug=True)
