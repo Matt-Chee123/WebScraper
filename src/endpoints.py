684f780d08ad923deb1c54d206feaaeca5635903
Matt-Chee123 <cheematthew10@gmail.com>
2024-09-05 18:36:32 +0100

    migrated fetch logic to google container

--- src/endpoints.py
+++ src/endpoints.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS  # Import the CORS module
+from flask_cors import CORS
 from bs4 import BeautifulSoup
 import requests
 import urllib.parse
@@ -15,6 +15,10 @@ import time
 app = Flask(__name__)
 CORS(app)
 
+chrome_options = Options()
+chrome_options.add_argument('--disable-notifications')
+s = Service('C:/Program Files/chromedriver-win64/chromedriver.exe')
+
 @app.route('/scrape/google', methods=['POST'])
 def scrapeGoogle():
 
@@ -23,20 +27,17 @@ def scrapeGoogle():
     if not query:
         return jsonify({'error': 'Google Query is required'}), 400
 
-    chrome_options = Options()
-    chrome_options.add_argument('--disable-notifications')
 
     # URL encode the query
     encoded_query = urllib.parse.quote_plus(query)
-    s = Service('C:/Program Files/chromedriver-win64/chromedriver.exe')
     driver = webdriver.Chrome(service=s, options=chrome_options)
-    time.sleep(2)
+    time.sleep(random.randint(1,3))
     driver.maximize_window()
     url=f'https://www.google.com/search?q=jobs%20london%20{encoded_query}&jbr=sep:0&udm=8&ved=2ahUKEwip7Iq026mIAxXFWUEAHTNoAccQ3L8LegQIIRAM'
     driver.get(url)
-    time.sleep(3)
+    time.sleep(random.randint(1,3))
     driver.find_element(By.XPATH,"//*[@id='W0wltc']/div").click()
-    time.sleep(3)
+    time.sleep(random.randint(1,3))
     for i in range(10):
         driver.execute_script('window.scrollBy(0, 3000)')
         time.sleep(random.randint(1,5))
@@ -57,3 +58,5 @@ def scrapeGoogle():
         return jsonify({'jobs': jobs})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
+
